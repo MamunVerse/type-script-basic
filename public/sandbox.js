19e5cf21155ff77d22e5de73a2a190bfcc870a93
Mamunur Rashid <49125977+MamunVerse@users.noreply.github.com>
2023-02-15 02:30:55 +0600

    OOP in typescript

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,5 +1,13 @@
-"use strict";
-let character = 'Mamun';
-let age = 30;
-let isBlackBelt = false;
-alert(character);
+import { Invoice } from './classes/Invoice.js';
+const invOne = new Invoice('mamun', 'work in js', 250);
+const invTwo = new Invoice('Rony', 'work on html', 200);
+let invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
+const form = document.querySelector('.new-item-form');
+//inputs
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,5 +1,26 @@
-let character = 'Mamun';
-let age = 30;
-let isBlackBelt = false;
 
-alert(character);  
+import { Invoice } from './classes/Invoice.js';
+
+const invOne = new Invoice('mamun', 'work in js', 250);
+const invTwo = new Invoice('Rony', 'work on html', 200);
+
+let invoices : Invoice[] = []
+
+invoices.push(invOne);
+invoices.push(invTwo);
+ 
+console.log(invoices);
+
+
+ 
+
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+//inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+   
